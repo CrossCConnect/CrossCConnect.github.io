c0ed5d1ff7a26e93558ca8b083692046d9532482
CrossCConnect <152434042+CrossCConnect@users.noreply.github.com>
2023-12-02 13:48:49 +0000

    feat!: Added signin, icons, comments, scrolling, and positioning
    Added many elements and their respective classed:
     - Added the "featured products" foundation for later testing
     - Added the two classes: featuredProductScroll, featuredProducts
     - Added account sign in html element
     - Added scrolling position and adjusted position of top elements
     accordingly
     - Added shopping cart icon

--- java.js
+++ java.js
@@ -1,8 +1,7 @@
+
 function click() {
     
 }
 function tick() {
-    if (document.getElementsByClassName("search").checked == True) {
-        document.getElementsByClassName("searchicon").style.borderWidth = "10px"
-    }
+    
 }
