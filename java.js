af01745da3fd104e7634c90993274d8aae38d86c
CrossCConnect <152434042+CrossCConnect@users.noreply.github.com>
2023-12-04 13:52:15 +0000

    feat!: Added scrolling to featured, javaScript(useless), and classes
    - Added scrolling to "featured products" and subsequent css
    - Added JavaScript that sadly, does not work. Will rework as of later
    -

--- java.js
+++ java.js
@@ -1,7 +1,13 @@
-
-function click() {
-    
-}
-function tick() {
-    
+let sectionsPassed = 0;
+const num_of_sections = 4;
+let sectionScroll = document.getElementById("featuredProductScroll");
+function autoScroll() {
+    if (sectionsPassed != num_of_sections) {
+        sectionScroll.scrollBy(100, 0);
+        sectionsPassed = sectionsPassed + 1;
+    } else {
+        sectionScroll.scrollBy(-400, 0);
+        sectionsPassed = 0;
+    }   
+    sectionScroll.scrollBy(window.innerHeight * 0.7, 100);
 }
